bfb4d0a9f6eb383e1f242618244e7d8f9a44d14f
Matthew James Stephenson <stephenson@Matthews-MacBook-Pro.local>
2024-02-03 16:23:37 -0500

    need to fix some operator stuff

--- lab5.py
+++ lab5.py
@@ -1,32 +1,37 @@
-# Lab5.py
+# Lab Exercise 5: lab5.py
+# 2/3/24
+# Matthew Stephenson
 # This program computes a person's car insurance based on their age and
-# the number of tickets they have received. The table for insurance
-# computation is given in Lab Assignment #3b.
-# STEP 1: Prompt the user for the person's age, read it in, convert to number
+# the number of tickets they have received.
 inscost = 0
+#initialized a var where I intend to reassign to a particular int as denoted by supplied table at canvas doc
 currency = '$'
+#added a currency ticker here to contat at instcost var to log to the standard i/o 
 age = int(input("Please enter the Customer's age: "))
-# STEP 2: Prompt the user for the person's number of tickets and read it in
+#at the wireframe / blueprint of this code supplied in canvas it was not condensed. made two lines one 
 tickets = int(input("Please enter the number of tickets: "))
+#did the same thing here as I did at line 11; there were two lines at this particular expression at the code wireframe supplied in canvas
 
 if age < 21:
+#imported logic from table at canvas document
     inscost = 1750*tickets
 elif age >= 21 and age <= 24:
     inscost = 1450*tickets
+#imported logic from table at canvas document
 elif age > 24:
     inscost = 1200*tickets
+#imported logic from table at canvas document. please see comments below the else statement
 else:
     print('Noooo....')
+#if none of the conditionals at the elif statements above are met this executes implying a program failure.
         
     
 print("\nGiven the data you've put in it's been determined that the customer is indeed", age, "years old.")
 print("\nI've also calculated their computed insurance cost to be", currency + str(inscost))
-# STEP 3: Compute the insurance cost based on the table given in the assignment
-# Step 4: Print out the resulting insurance cost message
-
-
+#Would have used the newline feature but canvas docs explicitly stated to print twice
 
 """
+TABLE CONVERSION / LOGIC
 RULE 1
 If Person's Age Is
 Less than 21 
@@ -48,3 +53,5 @@ Print a message explaining the person's age and number of tickets on one line, a
 on the next line showing the computed insurance cost. Include a $ sign before the cost when you print it
 out!
 """
+
+#Just realized this isn't done yet. Need to evaluate + $250 x number of tickets first AND THEN add $1500 to that. for each table computation. respectively.
