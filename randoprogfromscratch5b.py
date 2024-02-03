84e0ad6feddad9ceb948ce7f701bc06ca79147fc
Matthew James Stephenson <matthew.stephenson@uah.edu>
2024-02-03 16:59:46 -0500

    added randoprog; lecture 5B

---
+++ randoprogfromscratch5b.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  3 16:54:01 2024
+
+@author: stephenson
+"""
+
+grade = int(input("what grade did you get on the exam \n"))
+
+if grade >= 90:
+    print("yaaay")
+elif grade <= 90:
+    print("Noooo")
+else:
+    print("tf u smokin mf")
