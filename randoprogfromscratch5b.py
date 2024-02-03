01e533f6f5f6cdca1bc609d924cc5dc05dcdcc51
Matthew James Stephenson <matthew.stephenson@uah.edu>
2024-02-03 17:02:34 -0500

    refined program. actually coded this from scratch

--- randoprogfromscratch5b.py
+++ randoprogfromscratch5b.py
@@ -9,8 +9,8 @@ Created on Sat Feb  3 16:54:01 2024
 grade = int(input("what grade did you get on the exam \n"))
 
 if grade >= 90:
-    print("yaaay")
-elif grade <= 90:
-    print("Noooo")
+    print("yaaay; somehow you got an A.")
+elif grade >= 80:
+    print("a B? Surely you can do better than that")
 else:
-    print("tf u smokin mf")
+    print("u failed. sleep more. study more")
