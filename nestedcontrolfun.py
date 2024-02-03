e2b0f49d0962ae59395af9b658e7dcca30a20298
Matthew James Stephenson <matthew.stephenson@uah.edu>
2024-02-03 17:29:05 -0500

    wrote this from scratch lmao. untested

--- nestedcontrolfun.py
+++ nestedcontrolfun.py
@@ -8,3 +8,25 @@ Created on Sat Feb  3 17:14:18 2024
 #NESTED CONTROL STRUCT
 #seems like cell and tabular problems at grids can be broken down into decision trees
 #this is very interesting.
+tax_multiplier = 0
+#set empty var for taxrate multiplier. need to initiate it
+user_sal = int(input("mf what is your salary...200K Tc I hope"))
+user_stat = str(input("are you married? hope not. seems like a terrible deal.\n if so, type married and press enter"))
+#first prompt user for salary and filing status
+if  user_sal >= 100000 and user_stat == 'married':
+    tax_multiplier = 0.20
+    user_sal += 100000*tax_multiplier
+    print(user_sal)
+elif user_sal >= 100000 and user_stat != 'married':
+    tax_multiplier = 0.18
+    user_sal += 10000*tax_multiplier
+elif user_sal >= 50000 and user_stat == 'married':
+    tax_multiplier = 0.18
+    user_sal += 50000*tax_multiplier
+    print(user_sal)
+elif user_sal >= 50000 and user_stat != 'married':
+    tax_multiplier = 0.15
+    user_sal += 50000*tax_multiplier
+else:
+    user_sal += user_sal*0.10
+    print(user_sal)
