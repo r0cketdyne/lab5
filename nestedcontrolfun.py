b1bc9ce83e87f73b274f135798cc2b660b07362c
Matthew James Stephenson <matthew.stephenson@uah.edu>
2024-02-03 17:16:06 -0500

    decision tree

--- nestedcontrolfun.py
+++ nestedcontrolfun.py
@@ -6,3 +6,5 @@ Created on Sat Feb  3 17:14:18 2024
 @author: stephenson
 """
 #NESTED CONTROL STRUCT
+#seems like cell and tabular problems at grids can be broken down into decision trees
+#this is very interesting.
