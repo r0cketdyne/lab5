43d098222bb02182183b3e336ac405522fac12e0
Matthew James Stephenson <stephenson@Matthews-MacBook-Pro.local>
2024-02-03 15:32:52 -0500

    improved code readibility, deleted redundant code

--- lab5.py
+++ lab5.py
@@ -3,10 +3,8 @@
 # the number of tickets they have received. The table for insurance
 # computation is given in Lab Assignment #3b.
 # STEP 1: Prompt the user for the person's age, read it in, convert to number
-age_str = input("Please enter the Customer's age: ")
-age = int(age_str)
+age = int(input("Please enter the Customer's age: "))
 # STEP 2: Prompt the user for the person's number of tickets and read it in
-ticket_str = input("Please enter the number of tickets: ")
-tickets = int(ticket_str)
+ticket = int(input("Please enter the number of tickets: "))
 # STEP 3: Compute the insurance cost based on the table given in the assignment
 # Step 4: Print out the resulting insurance cost message
